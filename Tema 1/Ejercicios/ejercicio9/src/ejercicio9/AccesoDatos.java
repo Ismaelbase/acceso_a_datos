fd6190ebb1fde409096b81994399d56057092a85
Ismaelbase <ismaelbase93@gmail.com>
2022-09-28 12:34:52 +0200

    Ejercicio 9 completo

--- Tema 1/Ejercicios/ejercicio9/src/ejercicio9/AccesoDatos.java
+++ Tema 1/Ejercicios/ejercicio9/src/ejercicio9/AccesoDatos.java
@@ -13,78 +13,83 @@ import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.HashMap;
-
+import java.util.Iterator;
 
 public class AccesoDatos {
-    private HashMap<String,Pais> paises;
-    
-    public AccesoDatos(){
-        paises=new HashMap<>();
+
+    private HashMap<String, Pais> paises;
+
+    public AccesoDatos() {
+        paises = new HashMap<>();
     }
-    
+
     public String[] getColumnas() {
         return Pais.nombresAtributos();
     }
+
     // Ejercicio 1
-    public void cargarPaises(String nombre) throws FileNotFoundException,IOException,ClassNotFoundException{
+    public void cargarPaises(String nombre) throws FileNotFoundException, IOException, ClassNotFoundException {
         FileInputStream fis = new FileInputStream(nombre);
         ObjectInputStream ois = new ObjectInputStream(fis);
-        
-        while(fis.available()>0){
-            Pais p = (Pais)ois.readObject();
+
+        while (fis.available() > 0) {
+            Pais p = (Pais) ois.readObject();
             paises.put(p.getNombre(), p);
         }
-        
+
         ois.close();
         fis.close();
-        
+
     }
+
     // Ejercicio 6
-    public void guardarPaises(String nombre) throws IOException{
+    public void guardarPaises(String nombre) throws IOException {
         FileOutputStream fos = new FileOutputStream(nombre);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
-        
-        for(Pais p:this.paises.values()){
+
+        for (Pais p : this.paises.values()) {
             oos.writeObject(p);
         }
-        
+
         oos.close();
         fos.close();
-        
+
     }
+
     // Ejercicio 2
-    public ArrayList<String[]> mostrarTodo(){
+    public ArrayList<String[]> mostrarTodo() {
         ArrayList<Pais> todo = new ArrayList<>(this.paises.values());
-        
+
         return crearTabla(todo);
     }
+
     // Ejercicio 3
-    public void añadirPais(String nombre,String capital,int habitantes,int año){
-        
+    public void añadirPais(String nombre, String capital, int habitantes, int año) {
+
         Pais buscado = this.paises.get(nombre);
-        
-        if(buscado==null){
-            Pais nuevo = new Pais(nombre,capital,habitantes,año);
+
+        if (buscado == null) {
+            Pais nuevo = new Pais(nombre, capital, habitantes, año);
             this.paises.put(nombre, nuevo);
-        }else{
+        } else {
             throw new RuntimeException("Ya existe el pais que intentas insertar");
         }
     }
+
     // Ejercicio 4
-    public void backupTexto(String titulo) throws IOException{
+    public void backupTexto(String titulo) throws IOException {
         FileWriter fw = new FileWriter(titulo);
         PrintWriter pw = new PrintWriter(fw);
-        
-        String[] cabecera = {"nombre","año","habitantes","capital"};
-        
+
+        String[] cabecera = {"nombre", "año", "habitantes", "capital"};
+
         // Version normal Ej 4
-        
-        pw.println("Nombre,año,habitantes,capital");
-        
-        for(Pais p:this.paises.values()){
-            pw.println(p.getNombre()+","+p.getFundacion()+","+p.getHabitantes()+","+p.getCapital());
-        }
-        
+//        
+//        pw.println("Nombre,año,habitantes,capital");
+//        
+//        for(Pais p:this.paises.values()){
+//            pw.println(p.getNombre()+","+p.getFundacion()+","+p.getHabitantes()+","+p.getCapital());
+//        }
         // Version XML Ej 5
 //        
 //        pw.println("<mundo>");
@@ -100,13 +105,14 @@ public class AccesoDatos {
 //            pw.println("    </pais>");
 //        }
 //        pw.println(("</mundo>"));
-    
 //    
-    // Version JSON ej 5
-//    pw.println("\"paises\": {");
+        // Version JSON ej5 1
+//    pw.println("{ \n\"paises\": {");
 //    
 //    for(Pais p:this.paises.values()){
+//        
 //        for(int i=0;i<cabecera.length;i++){
+//            
 //            if(i!=cabecera.length-1){
 //                pw.println("\""+cabecera[i]+"\": \""+p.getNombre()+"\",");
 //            }else{
@@ -114,12 +120,44 @@ public class AccesoDatos {
 //            }   
 //        }   
 //    }
-//    pw.println("}");
+//    pw.println("}\n}");
+// Version JSON ej5 2
+        Iterator<Pais> iter = this.paises.values().iterator();
 
-      pw.close();
-      fw.close();
-}
-    
+        pw.println("{");
+
+        while (iter.hasNext()) {
+
+            Pais p = iter.next();
+            String[] valores = {p.getNombre(), String.valueOf(p.getFundacion()), String.valueOf(p.getHabitantes()), p.getCapital()};
+            pw.println("\n  \"" + p.getNombre() + "\": {");
+            if (iter.hasNext()) {
+                for (int i = 0; i < cabecera.length; i++) {
+                    if (i != cabecera.length - 1) {
+                        pw.println("        \"" + cabecera[i] + "\": \"" + valores[i] + "\",");
+                    } else {
+                        pw.println("        \"" + cabecera[i] + "\": \"" + valores[i] + "\"");
+                    }
+
+                }
+                pw.println("   },");
+            } else {
+                for (int i = 0; i < cabecera.length; i++) {
+                    if (i != cabecera.length - 1) {
+                        pw.println("        \"" + cabecera[i] + "\": \"" + valores[i] + "\",");
+                    } else {
+                        pw.println("        \"" + cabecera[i] + "\": \"" + valores[i] + "\"");
+                    }
+                }
+                pw.println("   }");
+            }
+
+        }
+        pw.println("}");
+        pw.close();
+        fw.close();
+    }
+
     private ArrayList<String[]> crearTabla(Collection<Pais> paises) {
         ArrayList<String[]> resultado = new ArrayList<>();
 
@@ -191,4 +229,4 @@ filein.close();
 dataIS.close();
 
 
-*/
+ */
